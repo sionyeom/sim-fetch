a6ba1ff1d804b5f00f75bb7447ada8cfa67786c0
sionyeom <duatldhs5555@gmail.com>
2024-08-08 23:51:44 +0900

    Chore: 불필요한 코드 삭제

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,3 @@ import { SimFetch } from './core/simFetch'; // Adjust the path if necessary
 import { ResponseHandlers } from './interfaces/ResponseHandlers';
 
 export { SimFetch, ResponseHandlers };
-
-try {
-  // 커스텀 헤더를 포함한 GET 요청
-  const customHeaders = {
-    Authorization: 'Bearer your-token',
-    'X-Custom-Header': 'custom-value',
-  };
-
-  const response = await SimFetch.get<any>(
-    'https://api.sampleapis.com/wines/reds',
-    customHeaders,
-  );
-
-  const { status, data } = response;
-
-  if (status === 200) {
-    return data;
-  }
-} catch (error) {
-  if (error instanceof SimFetchError) {
-    const { status, message } = error;
-  }
-}
