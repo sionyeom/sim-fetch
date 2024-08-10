a687603aacd89700903df07726b23500517664d5
sionyeom <duatldhs5555@gmail.com>
2024-08-10 11:45:24 +0900

    Comment: 주석 수정
    타입 명시 삭제

--- src/core/core.ts
+++ src/core/core.ts
@@ -18,13 +18,13 @@ type RequestMap = {
 const requests: RequestMap = {};
 
 /**
- * @desc HTTP 요청을 보내는 핵심 함수
+ * HTTP 요청을 보내는 핵심 함수
  * @template T
- * @param {string} url 요청할 URL
- * @param {HTTPMethod} method HTTP 메서드 (GET, POST, PATCH, DELETE)
- * @param {any} [body] 요청 본문 (POST, PATCH 요청 시 사용)
- * @param {HeadersInit} [customHeaders] 사용자 정의 헤더
- * @returns {Promise<{ data: T; status: number }>} 응답 데이터와 상태 코드
+ * @param url 요청할 URL
+ * @param method HTTP 메서드 (GET, POST, PATCH, DELETE)
+ * @param body 요청 본문 (POST, PATCH 요청 시 사용)
+ * @param customHeaders 사용자 정의 헤더
+ * @returns { data: T; status: number } 응답 데이터와 상태 코드
  * @throws {Error} 요청 중복 시 에러 발생
  */
 export const coreFetch = async <T>(
@@ -62,9 +62,9 @@ export const coreFetch = async <T>(
 /**
  * @desc HTTP 응답을 처리하는 함수
  * @template T
- * @param {string} url 요청한 URL
- * @param {Response} response fetch API 응답 객체
- * @returns {Promise<{ data: T; status: number }>} 응답 데이터와 상태 코드
+ * @param url 요청한 URL
+ * @param response fetch API 응답 객체
+ * @returns { data, status } 응답 데이터와 상태 코드
  * @throws {Error} 응답 상태가 OK가 아닌 경우 에러 발생
  */
 const handleResponse = async <T>(
@@ -88,8 +88,8 @@ const handleResponse = async <T>(
 
 /**
  * @desc 요청 처리 중 발생한 에러를 처리하는 함수
- * @param {string} url 요청한 URL
- * @param {Error} error 발생한 에러
+ * @param url 요청한 URL
+ * @param error 발생한 에러
  */
 const handleError = (url: string, error: Error): void => {
   cleanupRequest(url);
@@ -103,14 +103,14 @@ const handleError = (url: string, error: Error): void => {
 
 /**
  * @desc 특정 URL에 대한 요청이 진행 중인지 확인하는 함수
- * @param {string} url 요청할 URL
+ * @param url 요청할 URL
  * @returns {boolean} 요청 진행 여부
  */
 const isRequestInProgress = (url: string): boolean => !!requests[url];
 
 /**
  * @desc 새로운 AbortController를 생성하고 요청 추적에 추가하는 함수
- * @param {string} url 요청할 URL
+ * @param url 요청할 URL
  * @returns {AbortController} 생성된 AbortController
  */
 const createController = (url: string): AbortController => {
@@ -121,7 +121,7 @@ const createController = (url: string): AbortController => {
 
 /**
  * @desc 요청 추적에서 특정 URL의 항목을 제거하는 함수
- * @param {string} url 요청할 URL
+ * @param url 요청할 URL
  */
 const cleanupRequest = (url: string): void => {
   delete requests[url];
@@ -129,7 +129,7 @@ const cleanupRequest = (url: string): void => {
 
 /**
  * @desc 기본 헤더와 사용자 정의 헤더를 결합하여 최종 헤더를 생성하는 함수
- * @param {HeadersInit} [customHeaders] 사용자 정의 헤더
+ * @param customHeaders 사용자 정의 헤더
  * @returns {Headers} 결합된 헤더
  */
 const createHeaders = (customHeaders?: HeadersInit): Headers => {
--- src/core/simFetch.ts
+++ src/core/simFetch.ts
@@ -1,24 +1,24 @@
 import { coreFetch } from './core';
 
 /**
- * @desc SimFetch 클래스
+ * SimFetch 클래스
  * HTTP 요청을 쉽게 만들기 위한 유틸리티 클래스
  */
 export class SimFetch {
-  /** @type {HeadersInit} 기본 헤더 */
+  /** 기본 헤더 */
   private static defaultHeaders: HeadersInit = {};
 
   /**
    * 기본 헤더를 설정하는 메서드
-   * @param {HeadersInit} headers 설정할 헤더
+   * @param headers 설정할 헤더
    */
   static setDefaultHeaders(headers: HeadersInit): void {
     this.defaultHeaders = headers;
   }
 
   /**
-   * @desc 기본 헤더를 제거하는 메서드
-   * @param {string} key 제거할 헤더의 키
+   * 기본 헤더를 제거하는 메서드
+   * @param key 제거할 헤더의 키
    */
   static removeDefaultHeader(key: string): void {
     if (this.defaultHeaders instanceof Headers) {
@@ -31,13 +31,12 @@ export class SimFetch {
   }
 
   /**
-   * @desc GET 요청 메서드
-   * @template T
-   * @param {string} url 요청할 URL
-   * @param {object} [options] 쿼리 파라미터와 사용자 정의 헤더를 포함하는 옵션 객체
-   * @param {Record<string, string>} [options.params] 쿼리 파라미터 객체
-   * @param {HeadersInit} [options.customHeaders] 사용자 정의 헤더
-   * @returns {Promise<{ data: T; status: number }>} 응답 데이터와 상태 코드
+   * GET 요청 메서드
+   * @param url 요청할 URL
+   * @param options 쿼리 파라미터와 사용자 정의 헤더를 포함하는 옵션 객체
+   * @param options.params 쿼리 파라미터 객체
+   * @param options.customHeaders 사용자 정의 헤더
+   * @returns { data, status} 응답 데이터와 상태 코드
    */
   static async get<T>(
     url: string,
@@ -57,12 +56,11 @@ export class SimFetch {
   }
 
   /**
-   * @desc POST 요청 메서드
-   * @template T
-   * @param {string} url 요청할 URL
-   * @param {any} body 요청 본문
-   * @param {HeadersInit} [customHeaders] 사용자 정의 헤더
-   * @returns {Promise<{ data: T; status: number }>} 응답 데이터와 상태 코드
+   * POST 요청 메서드
+   * @param url 요청할 URL
+   * @param body 요청 본문
+   * @param customHeaders 사용자 정의 헤더
+   * @returns { data, status} 응답 데이터와 상태 코드
    */
   static async post<T>(
     url: string,
@@ -74,12 +72,11 @@ export class SimFetch {
   }
 
   /**
-   * @desc PATCH 요청 메서드
-   * @template T
-   * @param {string} url 요청할 URL
-   * @param {any} body 요청 본문
-   * @param {HeadersInit} [customHeaders] 사용자 정의 헤더
-   * @returns {Promise<{ data: T; status: number }>} 응답 데이터와 상태 코드
+   * PATCH 요청 메서드
+   * @param url 요청할 URL
+   * @param body 요청 본문
+   * @param customHeaders 사용자 정의 헤더
+   * @returns { data, status } 응답 데이터와 상태 코드
    */
   static async patch<T>(
     url: string,
@@ -91,12 +88,11 @@ export class SimFetch {
   }
 
   /**
-   * @desc DELETE 요청 메서드
-   * @template T
-   * @param {string} url 요청할 URL
-   * @param {any} [body] 요청 본문
-   * @param {HeadersInit} [customHeaders] 사용자 정의 헤더
-   * @returns {Promise<{ data: T; status: number }>} 응답 데이터와 상태 코드
+   * DELETE 요청 메서드
+   * @param url 요청할 URL
+   * @param body 요청 본문
+   * @param customHeaders 사용자 정의 헤더
+   * @returns { data, status } 응답 데이터와 상태 코드
    */
   static async delete<T>(
     url: string,
@@ -108,8 +104,8 @@ export class SimFetch {
   }
 
   /**
-   * @desc 헤더 병합 메서드
-   * @param {HeadersInit} [customHeaders] 사용자 정의 헤더
+   * 헤더 병합 메서드
+   * @param customHeaders 사용자 정의 헤더
    * @returns {HeadersInit} 병합된 헤더
    */
   private static mergeHeaders(customHeaders?: HeadersInit): HeadersInit {
@@ -127,9 +123,9 @@ export class SimFetch {
   }
 
   /**
-   * @desc URL에 쿼리 파라미터를 추가하는 메서드
-   * @param {string} url 기본 URL
-   * @param {Record<string, string>} [params] 쿼리 파라미터 객체
+   * URL에 쿼리 파라미터를 추가하는 메서드
+   * @param url 기본 URL
+   * @param params 쿼리 파라미터 객체
    * @returns {string} 쿼리 파라미터가 포함된 URL
    */
   private static buildUrlWithParams(
